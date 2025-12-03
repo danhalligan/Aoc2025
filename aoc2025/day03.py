a74d734bc6ef81c441efa93dc84e92f03f9a6598
Dan Halligan <dan.halligan@gmail.com>
2025-12-03 19:22:23 +0000

    Day 3

--- aoc2025/day03.py
+++ aoc2025/day03.py
@@ -1,9 +1,35 @@
-from aoc2025.aoc import Puzzle
-
-
 def part_a(data):
-    return None
+    tot = 0
+    for line in data.lines():
+        x = [int(i) for i in line]
+        m = max(x[:-1])
+        best = 0
+        for p, i in enumerate(x[:-1]):
+            if i == m:
+                r = int(str(i) + str(max(x[p + 1 :])))
+                if r >= best:
+                    best = r
+        tot += best
+    return tot
+
+
+def find_max(x, n):
+    """Find the maximum number that can be formed by selecting n+1 digits from x in order."""
+    if n == 0:
+        return max(x)
+    else:
+        m = max(x[:-n])
+        best = 0
+        for i, p in enumerate(x[:-n]):
+            if p == m:
+                r = find_max(x[i + 1 :], n - 1)
+                if r > best:
+                    best = r
+        return int(str(m) + str(best))
 
 
 def part_b(data):
-    return None
+    tot = 0
+    for line in data.lines():
+        tot += find_max([int(i) for i in line], 11)
+    return tot
