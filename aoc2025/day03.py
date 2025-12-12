861a55eda58e09c2d280c7aabc372bb045f6e922
Dan Halligan <dan.halligan@gmail.com>
2025-12-12 14:01:16 +0000

    Tweak

--- aoc2025/day03.py
+++ aoc2025/day03.py
@@ -1,19 +1,23 @@
+def join(a, b):
+    return int(f"{a}{b}")
+
+
 def part_a(data):
     tot = 0
     for line in data.lines():
         x = [int(i) for i in line]
         m = max(x[:-1])
-        tot += int(f"{m}{max(x[x.index(m) + 1 :])}")
+        tot += join(m, max(x[x.index(m) + 1 :]))
     return tot
 
 
 def find_max(x, n):
-    """Maximum number that can be formed by selecting n+1 digits from x in order."""
-    if n == 0:
+    """Maximum number that can be formed by selecting n digits from x in order."""
+    if n == 1:
         return max(x)
-    m = max(x[:-n])
-    return int(f"{m}{find_max(x[x.index(m) + 1:], n - 1)}")
+    m = max(x[: -(n - 1)])
+    return join(m, find_max(x[x.index(m) + 1 :], n - 1))
 
 
 def part_b(data):
-    return sum(find_max([int(i) for i in line], 11) for line in data.lines())
+    return sum(find_max([int(i) for i in line], 12) for line in data.lines())
